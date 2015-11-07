abf1b40aed8c9e6b8aa6efcee6246e6e06c421ca
Gabrielle Young <gabrielleyoung@Gabrielles-MacBook-Pro.local>
2015-11-08 08:50:09 +1300

    badge fix

--- app/controllers/tree_badges_controller.rb
+++ app/controllers/tree_badges_controller.rb
@@ -1,10 +1,11 @@
 class TreeBadgesController < ApplicationController
   def index
     @user = current_user
-    all_user_badges = TreeBadge.where(user_id: User.first) #TODO should be @user.id
+    all_user_badges = TreeBadge.where(user_id: current_user.id)
     # variable names:
+   @badge_names =  ForestTypesHelper::FOREST_TYPES.values.collect {|name| "@" + name.downcase.tr(" ", "_") + "_badges"}
 
-    @badge_names =  ["@fernland_badges", "@manuka_badges", "@podocarp_giants_badges", "@mangrove_badges", "@deciduous_hardwood_badges", "@nothofagus_beech_badges", "@exotic_badges"]
+    # @badge_names =  ["@fernland_badges", "@manuka_badges", "@podocarp_giants_badges", "@mangrove_badges", "@deciduous_hardwood_badges", "@nothofagus_beech_badges", "@exotic_badges"]
     ForestTypesHelper::FOREST_TYPES.each do |key,value|
       forest_tree_ids = Forest.find_by(name: value).trees.pluck(:id)#[1,2,3,4,5]
       name = value.downcase.tr(" ", "_")# podocarp_giant
@@ -16,7 +17,7 @@ class TreeBadgesController < ApplicationController
   def new
   	@badge = TreeBadge.new
   end
-  
+
   def create
     # TODO: update images to be stored in image table
     @user = current_user
