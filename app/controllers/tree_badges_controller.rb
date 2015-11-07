25ae782431b28adf7ae333c17fd7574d94bcec52
Gabrielle Young <gabrielleyoung@Gabrielles-MacBook-Pro.local>
2015-11-08 00:28:21 +1300

    add summary page

--- app/controllers/tree_badges_controller.rb
+++ app/controllers/tree_badges_controller.rb
@@ -4,8 +4,8 @@ class TreeBadgesController < ApplicationController
     @user = current_user
     all_user_badges = TreeBadge.where(user_id: User.first) #TODO should be @user.id
     # variable names:
-    @badge_names =  ["@fernland_badges", "@manuka_and_kanaka_badges", "@podocarp_giants_badges", "@mangrove_badges", "@deciduous_hardwood_badges", "@nothofagus_beech_badges", "@exotic_badges"]
-    @forest_names =  ["@fernland_badges", "@manuka_and_kanaka_badges", "@podocarp_giants_badges", "@mangrove_badges", "@deciduous_hardwood_badges", "@nothofagus_beech_badges", "@exotic_badges"]
+
+    @badge_names =  ["@fernland_badges", "@manuka_badges", "@podocarp_giants_badges", "@mangrove_badges", "@deciduous_hardwood_badges", "@nothofagus_beech_badges", "@exotic_badges"]
     ForestTypesHelper::FOREST_TYPES.each do |key,value|
       forest_tree_ids = Forest.find_by(name: value).trees.pluck(:id)#[1,2,3,4,5]
       name = value.downcase.tr(" ", "_")# podocarp_giant
